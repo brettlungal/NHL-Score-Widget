38d3cae1ed4c482c20de3024c6fd0fbce7893c2f
Brett L <43896341+brogrammer39@users.noreply.github.com>
2019-04-06 13:07:30 -0500

    Add files via upload

--- Widget.java
+++ Widget.java
@@ -13,7 +13,7 @@ import java.net.URL;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.Timer;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -25,6 +25,7 @@ import java.awt.Image;
 import javax.swing.JComboBox;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.JButton;
 
 public class Widget {
 
@@ -39,6 +40,7 @@ public class Widget {
 	int homeScore = 0;
 	int awayScore = 0;
 	boolean teamSelected = false;
+	private JButton exitButton;
 	// TODO return an array from http request and update global variables then
 	// update the labels ...how to loop? maybe move class to this file
 
@@ -79,10 +81,22 @@ public class Widget {
 		frame.setSize(300, 200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		ImageIcon img = new ImageIcon("img\\nhlLogo.png");
+		frame.setIconImage(img.getImage());
+		
+		JButton menuButton = new JButton("Menu");
+		menuButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		menuButton.setForeground(new Color(240, 248, 255));
+		menuButton.setBackground(new Color(47, 79, 79));
+		menuButton.setBounds(10, 11, 89, 23);
+		menuButton.setVisible(false);
+		
+		frame.getContentPane().add(menuButton);
 
 		JComboBox<ComboItem> comboBox = new JComboBox<ComboItem>();
-		comboBox.setBackground(Color.WHITE);
-		comboBox.setForeground(Color.BLACK);
+		comboBox.setBackground(new Color(47, 79, 79));
+		comboBox.setForeground(new Color(240, 248, 255));
 		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		comboBox.setEditable(true);
 		comboBox.setBounds(82, 79, 149, 32);
@@ -118,47 +132,45 @@ public class Widget {
 		comboBox.addItem(new ComboItem("Vegas Golden Knights", 54));
 		comboBox.addItem(new ComboItem("Washington Capitals", 15));
 		comboBox.addItem(new ComboItem("Winnipeg Jets", 52));
+		frame.getContentPane().add(comboBox);
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				comboBox.setVisible(false);
 				ComboItem choice = (ComboItem) comboBox.getSelectedItem();
 				teamId = choice.getId();
-				comboBox.setVisible(false);
-				System.out.println(teamId);
-				teamSelected = true;
-				setTeamInfo();
-				if (teamSelected) {
-					HTTPGetRequest(teamId);
-				}
+				setupTeamInfo();
+				menuButton.setVisible(true);
 			}
 		});
 
-		JLabel background = new JLabel(new ImageIcon("img\\resize.jpg"));
-		background.setBounds(167, 15, -1, -1);
-		frame.getContentPane().add(background);
-		background.setLayout(new FlowLayout());
-		frame.getContentPane().add(comboBox);
-
+		menuButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				hideTeamInfo();
+				comboBox.setVisible(true);
+			}
+		});
+		
 		// create labels for the scores and team names but dont show them yet
 		homeTeam = new JLabel(homeName);
 		homeTeam.setForeground(Color.WHITE);
-		homeTeam.setFont(new Font("Tahoma", Font.BOLD, 15));
-		homeTeam.setSize(139, 38);
-		homeTeam.setLocation(10, 23);
+		homeTeam.setFont(new Font("Tahoma", Font.BOLD, 14));
+		homeTeam.setSize(159, 38);
+		homeTeam.setLocation(0, 36);
 		homeTeam.setVisible(false);
 		frame.getContentPane().add(homeTeam);
 
 		awayTeam = new JLabel(awayName);
 		awayTeam.setForeground(Color.WHITE);
-		awayTeam.setFont(new Font("Tahoma", Font.BOLD, 15));
-		awayTeam.setLocation(157, 25);
-		awayTeam.setSize(133, 34);
+		awayTeam.setFont(new Font("Tahoma", Font.BOLD, 14));
+		awayTeam.setLocation(157, 34);
+		awayTeam.setSize(143, 34);
 		awayTeam.setVisible(false);
 		frame.getContentPane().add(awayTeam);
 
 		homeScoreDisplay = new JLabel(homeScore + "");
 		homeScoreDisplay.setForeground(Color.WHITE);
 		homeScoreDisplay.setFont(new Font("Tahoma", Font.BOLD, 90));
-		homeScoreDisplay.setLocation(191, 79);
+		homeScoreDisplay.setLocation(33, 79);
 		homeScoreDisplay.setSize(80, 80);
 		homeScoreDisplay.setVisible(false);
 		frame.getContentPane().add(homeScoreDisplay);
@@ -166,10 +178,24 @@ public class Widget {
 		awayScoreDisplay = new JLabel(awayScore + "");
 		awayScoreDisplay.setForeground(Color.WHITE);
 		awayScoreDisplay.setFont(new Font("Tahoma", Font.BOLD, 90));
-		awayScoreDisplay.setLocation(45, 79);
+		awayScoreDisplay.setLocation(187, 79);
 		awayScoreDisplay.setSize(80, 80);
 		awayScoreDisplay.setVisible(false);
 		frame.getContentPane().add(awayScoreDisplay);
+		
+		exitButton = new JButton("Exit");
+		exitButton.setForeground(new Color(240, 248, 255));
+		exitButton.setBackground(new Color(47, 79, 79));
+		exitButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		exitButton.setBounds(201, 12, 89, 23);
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.getContentPane().add(exitButton);
+		
+		
 
 		frame.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
@@ -184,14 +210,37 @@ public class Widget {
 
 			}
 		});
+		
+		Timer timer = new Timer(3000,new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if ( teamId != -1 ) {
+					HTTPGetRequest(teamId);
+				}
+				
+				
+			}
+		});
+		
+		timer.start();
 
 	}
+	
+	public void hideTeamInfo() {
+		homeTeam.setVisible(false);
+		awayTeam.setVisible(false);
+		homeScoreDisplay.setVisible(false);
+		awayScoreDisplay.setVisible(false);
+	}
 
-	public void setTeamInfo() {
+	public void setupTeamInfo() {
 		homeTeam.setVisible(true);
 		awayTeam.setVisible(true);
 		homeScoreDisplay.setVisible(true);
 		awayScoreDisplay.setVisible(true);
+	}
+
+	public void setTeamInfo() {
+		
 		homeTeam.setText(homeName);
 		homeScoreDisplay.setText(homeScore + "");
 		awayTeam.setText(awayName);
@@ -199,14 +248,11 @@ public class Widget {
 	}
 
 	public void HTTPGetRequest(int teamId) {
-		setTeamInfo();
-		while (true) {
-
+		
 			try {
 
-				String url = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + teamId; // testing with id 3, but
-																								// jets is
-				// id 52 dont forget to change
+				String url = "https://statsapi.web.nhl.com/api/v1/schedule?teamId=" + teamId;
+
 				URL obj = new URL(url);
 				HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -218,16 +264,22 @@ public class Widget {
 				String inputLine;
 				StringBuffer responce = new StringBuffer();
 				while ((inputLine = in.readLine()) != null) {
-					System.out.println(inputLine);
 					responce.append(inputLine);
 				}
 				in.close();
 
 				JSONObject myResponce = new JSONObject(responce.toString());
-				System.out.println("length: " + myResponce.length());
 
 				JSONArray dates = (JSONArray) myResponce.get("dates");
-				JSONObject games = (JSONObject) dates.get(0);
+				JSONObject games = null;
+				try {
+					games = (JSONObject) dates.get(0);
+				}catch(Exception noElement) {
+					//there is no game to pull stats from, so we leave the function
+					return;
+				}
+				
+				
 				JSONArray g = (JSONArray) games.get("games");
 				JSONObject gamePk = (JSONObject) g.get(0);
 				JSONObject teams = (JSONObject) gamePk.get("teams");
@@ -238,27 +290,20 @@ public class Widget {
 				homeName = ((JSONObject) homeTeam.get("team")).getString("name");
 				awayName = ((JSONObject) awayTeam.get("team")).getString("name");
 
-				System.out.println(homeTeam);
 				homeScore = homeTeam.getInt("score");
 				awayScore = awayTeam.getInt("score");
-
-				System.out.println(homeName + " vs. " + awayName);
-				System.out.println(homeName + ": " + homeScore);
-				System.out.println(awayName + ": " + awayScore);
+				System.out.println(homeName+" vs. "+awayName);
+				System.out.println(homeName+": "+homeScore);
+				System.out.println(awayName+": "+awayScore);
 
 				setTeamInfo();
+				
+				
 
-				try {
-					Thread.sleep(30000);
-				} catch (Exception e) {
-					System.out.println(e);
-				}
 			} catch (Exception c) {
-				System.out.println(c);
+				System.out.println(c.getMessage());
 			}
-			break;
-		}
+		
 
 	}
-
 }
